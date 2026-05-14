3e42b44ebc04b090921e6a616a5feb02056349b9
TugasConditionalStatement_0089 <rajhwaa@gmail.com>
2026-05-14 18:52:00 +0700

    menambahkan node baru di bagian depan

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -23,3 +23,9 @@ public:
     {
         top = NULL;
     }
+     int push(int value)
+    {
+        Node *newNode = new Node();
+        newNode->data = value;
+        newNode->next = top;
+        top = newNode;
